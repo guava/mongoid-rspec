c77a09792d4474bfd537a6afcab3fc0e49155591
Lailson Bandeira <lailson@guava.com.br>
2012-07-10 22:07:40 -0300

    Transform elements to array seems to be unecessary. (And wrong!)

--- lib/matchers/validations/inclusion_of.rb
+++ lib/matchers/validations/inclusion_of.rb
@@ -7,7 +7,7 @@ module Mongoid
         end
         
         def to_allow(*values)
-          @allowed_values = values.map(&:to_a).flatten
+          @allowed_values = values
           self
         end
         
